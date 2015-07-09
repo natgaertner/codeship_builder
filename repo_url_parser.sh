f3f68126352029c6135ae25af5ab1ea5f7a94374
Nathaniel Gaertner <natgaertner@gmail.com>
2015-07-09 16:06:29 -0700

    remove url parser (moved into build script)

--- repo_url_parser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git_repo_url=`git remote -v | grep fetch | grep -o -P 'git@.*?(?=\s)'`
-https_repo_url=`git remote -v | grep fetch | grep -o -P 'https://.*?(?=\s)'`
-if [ $https_repo_url ]
-  then
-    echo $https_repo_url
-  else
-    echo $git_repo_url | sed 's/:/\//g' | sed 's/^git@/https:\/\//' 
-fi
