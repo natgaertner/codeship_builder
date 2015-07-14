9774bfa6b89bf6828a7bafe8619635a87da486c8
Nathaniel Gaertner <natgaertner@gmail.com>
2015-07-14 15:42:02 -0700

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SERVICE_NAME=$1
+SERVICE_TASK_DEFINITION=$2
+aws ecs describe-task-definition --task-definition ${SERVICE_TASK_DEFINITION} | python -c "import sys,json; print json.dumps({'family':'${SERVICE_TASK_DEFINITION}', 'containerDefinitions':json.load(sys.stdin)['taskDefinition']['containerDefinitions']})" > tmp.json
+aws ecs register-task-definition --cli-input-json file://tmp.json
+rm tmp.json
+aws ecs update-service --service ${SERVICE_NAME} --task-definition ${SERVICE_TASK_DEFINITION}
+aws ecs wait services-stable --service ${SERVICE_NAME}
