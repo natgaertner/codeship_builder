2a52b367d08bec3350b01328fdec9093547bb3da
Nathaniel Gaertner <natgaertner@gmail.com>
2015-07-27 14:50:40 -0700

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 SERVICE_NAME=$1
-SERVICE_TASK_DEFINITION=$(aws ecs describe-services  --cluster ${ECS_CLUSTER:-default} --service ${SERVICE_NAME} | python -c 'import sys, json; print re.match(r"arn:aws:ecs:.*:\d+:task-definition/(?P<task_name>.*):\d+",json.load(sys.stdin)["services"][0][taskDefinition])')
+ECS_CLUSTER=$2
+SERVICE_TASK_DEFINITION=$(aws ecs describe-services  --cluster ${ECS_CLUSTER:-default} --service ${SERVICE_NAME} | python -c 'import sys, re, json; print re.match(r"arn:aws:ecs:.*:\d+:task-definition/(?P<task_name>.*):\d+",json.load(sys.stdin)["services"][0]["taskDefinition"]).group("task_name")')
+echo $SERVICE_TASK_DEFINITION
 aws ecs describe-task-definition --task-definition ${SERVICE_TASK_DEFINITION} | python -c "import sys,json; print json.dumps({'family':'${SERVICE_TASK_DEFINITION}', 'containerDefinitions':json.load(sys.stdin)['taskDefinition']['containerDefinitions']})" > tmp.json
 aws ecs register-task-definition --cli-input-json file://tmp.json
 rm tmp.json
