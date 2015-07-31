11cfbd6defe50649a702066f9fceeebc1d3dab4a
Nathaniel Gaertner <natgaertner@gmail.com>
2015-07-30 23:16:54 -0700

    set task def correctly in deply

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SERVICE_NAME=$1
 ECS_CLUSTER=$2
 SERVICE_TASK_DEFINITION=$(aws ecs describe-services  --cluster ${ECS_CLUSTER:-default} --service ${SERVICE_NAME} | python -c 'import sys, re, json; print re.match(r"arn:aws:ecs:.*:\d+:task-definition/(?P<task_name>.*):\d+",json.load(sys.stdin)["services"][0]["taskDefinition"]).group("task_name")')
 echo $SERVICE_TASK_DEFINITION
-aws ecs describe-task-definition --task-definition ${SERVICE_TASK_DEFINITION} | python -c "import sys,json; print json.dumps({'family':'${SERVICE_TASK_DEFINITION}', 'containerDefinitions':json.load(sys.stdin)['taskDefinition']['containerDefinitions']})" > tmp.json
+aws ecs describe-task-definition --task-definition ${SERVICE_TASK_DEFINITION} | python -c "import sys,json; d = json.load(sys.stdin)['taskDefinition']; d.pop('status');d.pop('taskDefinitionArn');d.pop('revision');print json.dumps(d)" > tmp.json
 aws ecs register-task-definition --cli-input-json file://tmp.json
 rm tmp.json
 aws ecs update-service --cluster ${ECS_CLUSTER:-default} --service ${SERVICE_NAME} --task-definition ${SERVICE_TASK_DEFINITION}
