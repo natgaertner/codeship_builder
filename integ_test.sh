311e8b860e2fb75cf4224e945b4cad4d48a15100
Nathaniel Gaertner <natgaertner@gmail.com>
2015-08-05 00:16:25 -0700

    make integ tests take task def and cluster params

--- integ_test.sh
+++ integ_test.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
-SERVICE_URL=$1
-taskArn=$(aws ecs run-task --cluster ${ECS_CLUSTER:-default} --task-definition ${TEST_TASK_DEFINITION} --overrides \
-"{ \
-\"containerOverrides\": \
-  [ \
-    { \
-    \"name\":\"tests\", \
-    \"environment\": \
-      [ \
-	{ \
-	\"name\": \"SERVICE_URL\", \
-	\"value\":\"$SERVICE_URL\" \
-	} \
-      ] \
-    } \
-  ] \
-}" | python -c "import sys, json; print json.load(sys.stdin)['tasks'][0]['taskArn']")
+TEST_TASK_DEFINITION=$1
+ECS_CLUSTER=$2
+taskArn=$(aws ecs run-task --cluster ${ECS_CLUSTER:-default} --task-definition ${TEST_TASK_DEFINITION} | python -c "import sys, json; print json.load(sys.stdin)['tasks'][0]['taskArn']")
 aws ecs wait tasks-stopped --cluster ${ECS_CLUSTER:-default} --tasks $taskArn
 aws ecs describe-tasks --cluster ${ECS_CLUSTER:-default} --task $taskArn
 _() { return $(aws ecs describe-tasks --cluster ${ECS_CLUSTER:-default} --task $taskArn | python -c "import sys, json; print json.load(sys.stdin)['tasks'][0]['containers'][0]['exitCode']"); }
