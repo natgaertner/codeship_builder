860a213eb58833870c089340139abacce7b17e07
Nathaniel Gaertner <natgaertner@gmail.com>
2015-08-10 14:59:17 -0700

    improve build wait

--- build.sh
+++ build.sh
@@ -33,7 +33,10 @@ taskArn=$(aws ecs run-task --cluster ${ECS_CLUSTER:-default} --task-definition $
     } \
   ] \
 }" | python -c "import sys, json; print json.load(sys.stdin)['tasks'][0]['taskArn']")
-aws ecs wait tasks-stopped --cluster ${ECS_CLUSTER:-default} --tasks $taskArn
+
+stopped=$(aws ecs describe-tasks --cluster ${ECS_CLUSTER:-default} --task $taskArn | python -c "import sys, json; print json.load(sys.stdin)['tasks'][0]['lastStatus']")
+{ while [ $stopped != 'STOPPED' ]; do sleep 10; stopped=$(aws ecs describe-tasks --cluster ${ECS_CLUSTER:-default} --task $taskArn | python -c "import sys, json; print json.load(sys.stdin)['tasks'][0]['lastStatus']"); printf '.'; done; } | timeout 1200 cat
+
 aws ecs describe-tasks --cluster ${ECS_CLUSTER:-default} --task $taskArn
 _() { return $(aws ecs describe-tasks --cluster ${ECS_CLUSTER:-default} --task $taskArn | python -c "import sys, json; print json.load(sys.stdin)['tasks'][0]['containers'][0]['exitCode']"); }
 _
